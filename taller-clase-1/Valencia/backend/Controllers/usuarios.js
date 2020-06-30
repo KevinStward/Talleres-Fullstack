a17e8878b53cae11c813b2612bfd8c4c1ac35e12
Kevin Estupinan <keffstward@gmail.com>
2020-06-30 03:01:07 -0500

    Valencia-Usuarios: Editar y Eliminar

--- taller-clase-1/Valencia/backend/Controllers/usuarios.js
+++ taller-clase-1/Valencia/backend/Controllers/usuarios.js
@@ -53,6 +53,102 @@ var controller={
             })
         })
     },
+    update: (req, res) => {
+        //Recoger los parametros
+        var params = req.body;
+        //Validar los datos que nos envian
+        try {
+            var validar_nombres=validation.isEmpty(params.nombres);
+            var validar_apellidos=validation.isEmpty(params.apellidos);
+            var validar_correo=validation.isEmpty(params.correo);
+            var validar_contrasenna=validation.isEmpty(params.contrasenna);
+        }
+        catch (err) {
+            return res.status(400).send({
+                status: 'error',
+                message:"Rectifique los campos",
+            });
+        };
+        //Validar si el id ingresado existe
+        var id = req.params.id;
+        console.log(id);
+        if (!id || id == null) {
+            return res.status(404).send({
+                status: 'error',
+                message: 'No se ingreso ingun id',
+            });
+        }
+        Usuario.findById(id, (err, userFound) => {
+            if (err) {
+                return res.status(404).send({
+                    status: 'error',
+                    message: 'No se pudo procesar',
+                });
+            }
+            if (!userFound) {
+                return res.status(500).send({
+                    status: 'error',
+                    message: 'No existe el estudiante con el id ' + id,
+                })
+            }
+            Usuario.updateOne(userFound, params, (err, userUpdated) => {
+                if (err) {
+                    return res.status(404).send({
+                        status: 'error',
+                        message: 'No se pudo procesar',
+                    });
+                }
+                if (!userUpdated) {
+                    return res.status(500).send({
+                        status: 'error',
+                        message: 'No existe el estudiante con el id ' + id,
+                    })
+                }
+                return res.status(200).send({
+                    status: 'Registro actualizado con exito',
+                    message: userUpdated,
+                })
+            })
+
+        })
+    },
+
+    delete:(req,res) =>{
+        var id = req.params.id;
+        if (!id || id == null) {
+            return res.status(404).send({
+                status: 'error',
+                message: 'No se ingreso ingun id',
+            });
+        }
+        //Buscar el id
+        Usuario.findById(id, (err, userFound) => {
+            console.log(userFound);
+            if (err) {
+                return res.status(404).send({
+                    status: 'error',
+                    message: 'No se pudo procesar',
+                });
+            }
+            if (!userFound) {
+                return res.status(500).send({
+                    status: 'error',
+                    message: 'No existe el estudiante con el id ' + id,
+                })
+            }
+            Usuario.deleteOne(userFound, (err) => {
+                if (err) {
+                    return res.status(404).send({
+                        status: 'error',
+                        message: 'No se pudo procesar',
+                    });
+                }
+                return res.status(200).send({
+                    status: 'Eliminación completa',
+                })
+            });
+        });   
+    },
 } 
 
 module.exports=controller;
--- taller-clase-1/Valencia/backend/Routes/usuarios.js
+++ taller-clase-1/Valencia/backend/Routes/usuarios.js
@@ -5,4 +5,7 @@ var router=express.Router();
 //router.get('/usuarios',personaController.datosUsuario);
 router.post('/guardarUsuarios',personaController.save);
 router.get('/listarUsuarios',personaController.get_Usuarios);
+router.put('/actualizarUsuario/:id',personaController.update);
+router.delete('/borrarUsuario/:id',personaController.delete);
+
 module.exports=router;
